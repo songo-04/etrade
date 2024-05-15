3de89de42cc42d7be9ff6072f1e9344f1f3646cb
vincent <119781833+songo-04@users.noreply.github.com>
2024-05-15 15:52:22 +0000

    change file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(`message`);
+import express from 'express';
+import cors from 'cors';
+import mongooose from 'mongoose';
+import {MONGO_URI} from './config.mjs';
+const app = express();
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+  console.log(`Server is running on port ${PORT}`);
+})
