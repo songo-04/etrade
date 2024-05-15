18ba147435585eb71c8c65ad590c9595dc7f56fa
vincent <119781833+songo-04@users.noreply.github.com>
2024-05-15 16:31:44 +0000

    user controller

---
+++ route/user.mjs
@@ -0,0 +1,7 @@
+import express from 'express'
+import { signin } from '../controller/user.mjs'
+const route_user = express.Router()
+route_user.post('/signin', signin)
+route_user.post('/signup', signup)
+
+export default route_user
