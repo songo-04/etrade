01d91fd500c9f1132f0239881554889c519e8b69
vincent <119781833+songo-04@users.noreply.github.com>
2024-05-15 15:58:40 +0000

    indejx change added mongoose connection

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-import express from 'express';
-import cors from 'cors';
-import mongooose from 'mongoose';
-import {MONGO_URI} from './config.mjs';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { MONGO_URI, PORT } from "./config.mjs";
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-})
+});
