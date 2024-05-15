fce0bd4363949e1469dc17d15cd21018ea3abce6
vincent <119781833+songo-04@users.noreply.github.com>
2024-05-15 16:35:38 +0000

    change index to add user route

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import { MONGO_URI, PORT } from "./config.mjs";
+import route_user from "./route/user.mjs";
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use("/auth", route_user);
 
 mongoose
   .connect(MONGO_URI)
