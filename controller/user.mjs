d843a25873d52977b016f434d60d473a03386cda
vincent <119781833+songo-04@users.noreply.github.com>
2024-05-15 16:22:53 +0000

    user controller changed

--- controller/user.mjs
+++ controller/user.mjs
@@ -1,21 +1,39 @@
 import User from "../model/user.mjs";
 import bcryptjs from "bcryptjs";
 
-async function signup(req, res,next){
-  try{
-    const{user_name,user_email,user_password} = req.body;
-    if(!user_name || !user_email || !user_password){
+async function signup(req, res, next) {
+  try {
+    const { user_name, user_email, user_password } = req.body;
+    if (!user_name || !user_email || !user_password) {
       return res.json('Please provide all the required fields')
     }
-    const hashPassword = await bcryptjs.hash(user_password,10);
-    const user = new User({user_name,user_email,user_password:hashPassword});
+    const hashPassword = await bcryptjs.hash(user_password, 10);
+    const user = new User({ user_name, user_email, user_password: hashPassword });
     await user.save();
-    res.status(201).json({message:'User created successfully',user:user})
-  }catch(err){next(err)}
+    res.status(201).json({ message: 'User created successfully', user: user })
+  } catch (err) { next(err) }
 }
-async function signin(req, res,next){}
-async function signout(req, res,next){}
-async function update(req, res,next){}
-async function deleteUser(req, res,next){}
+async function signin(req, res, next) {
 
-export {signin,signup,signout,update,deleteUser}
+  try {
+    const { user_email, user_password } = req.body;
+    if (!user_name || !user_password) {
+      return res.json('Please provide all the required fields')
+    }
+    const isUserValide = await User.findOne({ user_email });
+    if (!isUserValide) {
+      return res.json('user email not found')
+    }
+    const isPasswordValide = await bcryptjs.compare(user_password, isUserValide.user_password);
+    if (!isPasswordValide) {
+      return res.json('user password not found')
+    }
+    res.status(200).json({ message: 'User login successfully', user: isUserValide })
+
+  } catch (err) { next(err) }
+}
+async function signout(req, res, next) { }
+async function update(req, res, next) { }
+async function deleteUser(req, res, next) { }
+
+export { signin, signup, signout, update, deleteUser }
