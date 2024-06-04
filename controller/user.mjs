6181c504d730ee20cebb1c52b062746d066ad584
vincent <119781833+songo-04@users.noreply.github.com>
2024-06-04 04:23:01 +0000

    User controller change

--- controller/user.mjs
+++ controller/user.mjs
@@ -1,16 +1,17 @@
 import User from "../model/user.mjs";
 import bcryptjs from "bcryptjs";
 import jwt from "jsonwebtoken";
+import {SECRET_KEY} from "./../config.mjs";
 
 async function signup(req, res, next) {
   try {
     const { user_name, user_email, user_password } = req.body;
     const userckeck = await User.findOne({ user_email });
-    if (userckeck){return res.status(400).json({message: "Email already exists"})}
+    if (userckeck) { return res.status(400).json({ message: "Email already exists" }) }
     if (!user_name || !user_email || !user_password) {
       return res.json('Please provide all the required fields')
     }
-    const hashPassword = await bcryptjs.hash(user_password, 10);
+    const hashPassword = bcryptjs.hashSync(user_password, 10);
     const user = new User({ user_name, user_email, user_password: hashPassword });
     await user.save();
     res.status(201).json({ message: 'User created successfully', user: user })
@@ -23,17 +24,18 @@ async function signin(req, res, next) {
     if (!user_email || !user_password) {
       return res.json('Please provide all the required fields')
     }
-    const isUserValide = await User.findOne({ user_email });
+    const isUserValide = await User.findOne({ user_email: user_email });
     if (!isUserValide) {
       return res.json('user email not found')
     }
-    const isPasswordValide = await bcryptjs.compare(user_password, isUserValide.user_password);
+    const isPasswordValide = bcryptjs.compareSync(user_password, isUserValide.user_password);
     if (!isPasswordValide) {
       return res.json('user password not found')
     }
-    const token = jwt.sign({ user_id: isUserValide._id }, process.env.JWT_SECRET, { expires: 60 * 60 })
+    const token = jwt.sign({ user_id: isUserValide._id },SECRET_KEY, {expiresIn: '1h'})
 
-    res.status(200).json({ message: 'User login successfully', user: isUserValide }).cookies('token', token, { httpOnly: true })
+    res.status(200).json({ message: 'User login successfully', user: isUserValide ,token: token })
+    res.status(200).cookie('token', token,{httpOnly: true, maxAge: 3600000})
 
   } catch (err) { next(err) }
 }
